16bd869eb6d0c3a3a287871907db39ac84a9db40
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-04-13 12:42:04 -0300

    Correções no carregamento da biblioteca mqtt
    - Correção de erro na biblioteca mqtt que causava
      erro de import por causa de nome errado
    
    - Correção de error que causava a configuração
      errada do python PATH pela bilioteca mqtt

--- mqtt/__init__.py
+++ mqtt/__init__.py
@@ -2,14 +2,20 @@
 # exporta o caminho da bilioteca para o python PATH
 import os, sys
 basepath = os.path.split(__file__)[0]
-sys.path.insert(0, basepath)
+dirpath  = os.path.split(basepath)[0]
+
+if dirpath in sys.path:
+	sys.path.remove(dirpath)
+
+sys.path.insert(0, dirpath)
 
 # limpeza
 del os, sys
 del basepath
+del dirpath
 
 # modulos da blibioteca disponives por padrao
-from mqtt import cliente
+from mqtt import client
 
 # configura o que eh visivel fora da bilioteca
-__all__ = ["cliente"] + cliente.__all__
+__all__ = ["cliente"] + client.__all__
