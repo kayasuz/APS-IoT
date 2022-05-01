9ddbc58750af26c2adc674ec44cca2c44459d201
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-05-01 16:16:46 -0300

    Execução da biblioteca mqtt
    Adição de função principal que inicia um cliente mqtt
    e conecta com o Arduino pela porta USB automaticamente

--- mqtt/__main__.py
+++ mqtt/__main__.py
@@ -3,12 +3,45 @@
 
 # modulos necessarios
 from mqtt.client import MQTTClient
-
+from mqtt.serial import SerialHandler
 
 # ------------ corpo ------------
 
+broker = "localhost"
+
+def on_message(client, topico, mensagem):
+    print(f"{topico}: {mensagem}")
+
 def main():
-    pass
+    import sys
+    try:
+        client = MQTTClient(broker)
+    except ConnectionRefusedError:
+        print(f"falha ao conectar ao broker: conexao recusada", file=sys.stderr)
+        exit(1)
+
+    if sys.platform == "linux":
+        porta = "/dev/ttyACM0"
+    elif sys.platform.lower().startswith("windows"):
+        porta = "COM5"
+    else:
+        print(f"plataforma nao suportada")
+        exit(1)
+
+    handler = SerialHandler(client, porta)
+    client.message_callback(on_message)
+
+    handler.encaminhar("temperature", "/dev/sensor/temp:0")
+    handler.encaminhar("humidity",    "/dev/sensor/humi:0")
+
+    client.subscribe("/dev/sensor/temp:0")
+    client.subscribe("/dev/sensor/humi:0")
+
+    client.loop_start()
+    try:
+        handler.loop_forever()
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == "__main__":
     main()
